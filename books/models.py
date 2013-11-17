12d8df744ce6782617e44740d019499328d70613
Adam Davis <davisadam10@googlemail.com>
2013-11-17 21:13:33 +0000

    added files and changes

--- books/models.py
+++ books/models.py
@@ -9,6 +9,9 @@ class Publisher(models.Model):
     country = models.CharField(max_length=50)
     website = models.URLField()
 
+    def __str__(self):
+        return self.name
+
 
 class Author(models.Model):
     salutation = models.CharField(max_length=10)
